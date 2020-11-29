e2fb42dd194d65503ea7380abd02b0a29015d9fe
Dino Spumony <aspumony@gmail.com>
2020-11-29 18:49:57 +0200

    Added localization

--- src/app.js
+++ src/app.js
@@ -1,5 +1,4 @@
 import React from 'react';
-
 import { Container } from 'reactstrap';
 import Footer from './common/components/footer/footer';
 import Header from './common/components/header/header';
@@ -8,9 +7,22 @@ import Projects from './common/components/projects/projects';
 import Skills from './common/components/skills/skills';
 import Work from './common/components/work/work';
 
+import { useTranslation, Trans } from 'react-i18next';
+
 const App = () => {
+  const { t, i18n } = useTranslation();
+
+  const changeLanguage = (lng) => {
+    i18n.changeLanguage(lng);
+  };
+
   return (
     <Container>
+      <h2>{t('Welcome to React')}</h2>
+      <button onClick={() => changeLanguage('ru')}>ru</button>
+      <button onClick={() => changeLanguage('en')}>en</button>
+      <Trans>Welcome</Trans>
+
       <Navbar />
       <Header />
       <Skills />
